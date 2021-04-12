9310b543b438687bc1cf4633bff220c2e8c229ce
ekong8 <dmsql9270@gmail.com>
2021-04-11 23:18:50 -0400

    config update

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,49 +1,54 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ActivatedRoute, Params } from '@angular/router';
-
-import { MatSnackBar } from '@angular/material/snack-bar';
-
-import { MusicDataService } from '../music-data.service';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ActivatedRoute } from "@angular/router";
+import { MusicDataService } from "../music-data.service";
 
 @Component({
-  selector: 'app-album',
-  templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css'],
+  selector: "app-album",
+  templateUrl: "./album.component.html",
+  styleUrls: ["./album.component.css"],
 })
-export class AlbumComponent implements OnInit, OnDestroy {
-  album: any;
-  albumSubscription: Subscription;
+export class AlbumComponent implements OnInit {
+  album;
+  private sub;
+  private liveDataSub;
+  id;
 
   constructor(
-    private snackBar: MatSnackBar,
+    @Inject(MatSnackBar) public data2: string,
     private route: ActivatedRoute,
-    private dataService: MusicDataService
+    private dataService: MusicDataService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
-    this.albumSubscription = this.route.params.subscribe((params: Params) => {
-      this.dataService
-        .getAlbumById(params.id)
-        .subscribe((data) => (this.album = data));
+    this.sub = this.route.params.subscribe((params) => {
+      this.id = params["id"];
+      this.liveDataSub = this.dataService
+        .getAlbumById(this.id)
+        .subscribe((data) => {
+          {
+            this.album = data;
+          }
+
+        });
     });
   }
-
-  ngOnDestroy(): void {
-    if (this.albumSubscription) {
-      this.albumSubscription.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    this.liveDataSub.unsubscribe();
   }
 
-  addToFavourites(trackId): void {
-    this.dataService.addToFavourites(trackId).subscribe(
+  addToFavourites(trackId: any) {
+    this.liveDataSub = this.dataService.addToFavourites(trackId).subscribe(
       (data) => {
-        this.snackBar.open('Adding to Favourites...', 'Done', {
+
+        this.snackBar.open("Adding to favourites...", "Done", {
           duration: 1500,
         });
       },
       (err) => {
-        this.snackBar.open('Unable to add song to Favourites...', 'Done', {
+        this.snackBar.open("Unable to add song to favourites...", "Done", {
           duration: 1500,
         });
       }
